6dd4889695bb174e9b9cda9e66269f0696031e54
Stalkermtx <106044847+Stalkermtx@users.noreply.github.com>
2024-02-07 19:53:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ roxo="\e[38;2;128;0;128m"
 reset="\e[0m"
 
 rm -rf /root/StartNet/
-rm -f /usr/local/bin/ulekCheckuser
-pkill -9 -f "/root/StartNetCheckUser/checkuser.py"
+rm -f /usr/local/bin/StartNet
+pkill -9 -f "/root/StartNet/checkuser.py"
 
 apt update && apt upgrade -y && apt install python3 git -y
 git clone https://github.com/Stalkermtx/StartNetCheckUser.git
-chmod +x /root/StartNetCheckUser/checkuserMenu.sh
+chmod +x /root/StartNet/checkuserMenu.sh
 ln -s /root/StartNet/checkuserMenu.sh /usr/local/bin/StartNetCheckuser
 
 clear
